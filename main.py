675a88d34d9d0397bc3622c3d4938fe2bf44c550
FemkeBakker <femke.bakker02@gmail.com>
2023-01-11 12:34:41 +0100

    Add code to load data

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # CASE: RailNL - Team: RailNL-STF
 
-# load data Deel 1 - Noord-Holland & Zuid-Holland
+#libraries
+import pandas as pd
 
+# load data Deel 1 - Noord-Holland & Zuid-Holland
+ConnectiesHolland = pd.read_csv("Data-deel1/ConnectiesHolland.csv")
+StationsHolland = pd.read_csv("Data-deel1/StationsHolland.csv")
+
+# load data deel 2 - Heel NL
+ConnectiesNationaal = pd.read_csv("Data-deel2/ConnectiesNationaal.csv")
+StationsNationaal = pd.read_csv("Data-deel2/StationsNationaal.csv")
