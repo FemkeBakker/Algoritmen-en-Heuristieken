de8cb32c28fe0d264a91f3e9b1cc3b844ae1064f
FemkeBakker <femke.bakker02@gmail.com>
2023-01-12 13:49:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,28 @@ StationsHolland = pd.read_csv("Data-deel1/StationsHolland.csv")
 # load data deel 2 - Heel NL
 ConnectiesNationaal = pd.read_csv("Data-deel2/ConnectiesNationaal.csv")
 StationsNationaal = pd.read_csv("Data-deel2/StationsNationaal.csv")
+<<<<<<< HEAD
+
+# all stations in NH & NZ
+# stations_Holland = set(StationsNationaal['station'])
+
+#classes: stations, verbindingen, trajecten
+
+class Stations:
+    def __init__(self, station, x=-300, y=-300):
+        self.station = station
+        self.x = x
+        self.y = y
+
+# for row in StationsHolland.rows:
+#     print(row)
+
+
+
+
+
+
+sloterdijk = Stations("Amsterdam Sloterdijk")
+print(sloterdijk.station, sloterdijk.x)
+
+    
