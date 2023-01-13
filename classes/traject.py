6732da28661c808e3a067314b15bf4ca3a7a3bef
FemkeBakker <femke.bakker02@gmail.com>
2023-01-13 15:37:19 +0100

    traject & main

---
+++ classes/traject.py
@@ -0,0 +1,14 @@
+# example traject
+traject = {"connecties" : ["Beverwijk", "Castricum", "Alkmaar", "Hoorn", "Zaandam"], "min": 100, "all_connections":28}
+
+class Traject():
+    def __init__(self, traject, uid):
+        self.uid = uid # give id to traject
+        self.connecties = traject['connecties']
+        self.min = traject['min'] # total minutes of traject
+        self.p = len(self.connecties) / traject['all_connections'] # fraction of stations in traject
+
+
+
+
+
--- main.py
+++ main.py
@@ -15,23 +15,11 @@ StationsNationaal = pd.read_csv("Data-deel2/StationsNationaal.csv")
 # all stations in NH & NZ
 # stations_Holland = set(StationsNationaal['station'])
 
-#classes: stations, graph, trajecten
+#classes: stations, graph
 
 from classes.station import Station
 
-# alkmaar = Station("Alkmaar", ConnectiesHolland)
-# print(alkmaar.connection_count)
 
-# class Graph():
-#     def __init__(self, df_connecties, df_stations):
-#         self.stations = self.get_stations(df_stations)
-
-
-#     def get_stations(self, df_stations):
-#         return {station:station for station in df_stations['station'].values}
-
-# graaf = Graph(ConnectiesHolland, StationsHolland)
-# print(graaf.stations)
 
 
 
