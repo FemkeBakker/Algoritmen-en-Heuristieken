978617bc62cd00400a61b3af95f55b2780320dad
FemkeBakker <femke.bakker02@gmail.com>
2023-01-13 13:38:19 +0100

    class station: removed unneccesary code

--- classes/station.py
+++ classes/station.py
@@ -4,19 +4,6 @@
 import pandas as pd
 import numpy as np 
 
-# load data Deel 1 - Noord-Holland & Zuid-Holland
-ConnectiesHolland = pd.read_csv("Data-deel1/ConnectiesHolland.csv")
-StationsHolland = pd.read_csv("Data-deel1/StationsHolland.csv")
-
-# load data deel 2 - Heel NL
-ConnectiesNationaal = pd.read_csv("Data-deel2/ConnectiesNationaal.csv")
-StationsNationaal = pd.read_csv("Data-deel2/StationsNationaal.csv")
-
-# all stations in NH & NZ
-# stations_Holland = set(StationsNationaal['station'])
-
-#classes: stations, graph, trajecten
-
 class Station:
     def __init__(self, station, df_connecties, x=np.nan, y=np.nan):
         self.station = station
